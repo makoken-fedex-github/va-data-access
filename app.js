8859e2bb1b84d667f1cd7cb7b9e655b01d322597
makoken-fedex-github <127207881+makoken-fedex-github@users.noreply.github.com>
2023-03-08 16:30:36 +0100

    shipmentType and shipmentPostalCode properties added.

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.get('/', (req, res) => {
 app.get('/pickup', (req, res) => {
   const result = {
     returnCode: 0,
-    coffee_price_value: 2.55
+    coffee_price_value: 2.55,
+    shipmentType: "international",
+    shipmentPostalCode: "1112 XR"
   };
   res.setHeader('Content-Type', 'application/json');
   res.json(result);
