a9e6013c629ba85bd4311383cd0f1398ab248ab6
Bram Willemse (OSV) <bram.willemse.osv@fedex.com>
2023-03-14 14:29:52 +0100

    Remove coffee and correct alphabetize

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ app.get('/', (req, res) => {
 app.get('/pickup', (req, res) => {
   const result = {
     accountType: "individual", // options: ["individual", "business"]
-    coffee_price_value: 2.55,
     isLoggedIn: true, // boolean
     returnCode: 0,
-    shipmentType: "international", // eligible for pickup: ["domestic", "return", "international"]. uneligible: ["freight", "dangerous"]
     shipmentPostalCode: "1000AA", // options: Dutch zip codes (regex: ^\d{4}[A-Za-z]{2}$ )
+    shipmentType: "international", // eligible for pickup: ["domestic", "return", "international"]. uneligible: ["freight", "dangerous"]
   };
   res.setHeader('Content-Type', 'application/json');
   res.json(result);
