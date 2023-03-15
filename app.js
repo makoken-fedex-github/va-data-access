d82198ae362e8cfd9ea59511da59a6efc4eb3ab4
makoken-fedex-github <127207881+makoken-fedex-github@users.noreply.github.com>
2023-03-15 21:27:26 +0100

    if statement added.

--- app.js
+++ app.js
@@ -12,12 +12,29 @@ app.get('/pickup', (req, res) => {
     isLoggedIn: true, // boolean
     returnCode: 0,
     shipmentPostalCode: "1000AA", // options: Dutch zip codes (regex: ^\d{4}[A-Za-z]{2}$ )
-    shipmentType: "international", // eligible for pickup: ["domestic", "return", "international"]. uneligible: ["freight", "dangerous"],
-    reqObjDump: JSON.stringify(req)
+    shipmentType: "international", // eligible for pickup: ["domestic", "return", "international"]. uneligible: ["freight", "dangerous"]
   };
   res.setHeader('Content-Type', 'application/json');
   res.json(result);
 });
+
+app.post('/pickup', (req, res) => {
+  const { trackingNumber } = req.body;
+  let result = {
+    accountType: 'individual',
+    isLoggedIn: true,
+    returnCode: 0,
+    shipmentPostalCode: '1000AA',
+    shipmentType: 'international',
+  };
+  if (trackingNumber && trackingNumber.startsWith('111')) {
+    result.shipmentPostalCode = '2000AB';
+  }
+  res.setHeader('Content-Type', 'application/json');
+  res.json(result);
+});
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
