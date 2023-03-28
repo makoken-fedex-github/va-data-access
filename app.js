209e1bca0d502c1be756a7e1674d659aa5139a52
makoken-fedex-github <127207881+makoken-fedex-github@users.noreply.github.com>
2023-03-28 08:31:53 +0200

    sendemail function added.

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
+var nodemailer = require('nodemailer');
+
+
 const express = require('express');
 const app = express();
+
+function sendEmail(){
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'youremail@gmail.com',
+      pass: 'yourpassword'
+    }
+  });
+
+  var mailOptions = {
+    from: 'youremail@gmail.com',
+    to: 'myfriend@yahoo.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  }); 
+}
+
 app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello, world!');
