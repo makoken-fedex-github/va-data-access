555856161fcb243b88a74215917ddc24a839abad
Bram Willemse (OSV) <bram.willemse.osv@fedex.com>
2023-05-03 15:40:44 +0200

    Optimize documentation, cleanup unused code

--- app.js
+++ app.js
@@ -1,195 +1,172 @@
-
-var nodemailer = require('nodemailer');
-const axios = require('axios');
+var nodemailer = require('nodemailer')
+const axios = require('axios')
 const Sib = require('sib-api-v3-sdk')
 const client = Sib.ApiClient.instance
 const apiKey = client.authentications['api-key']
 apiKey.apiKey = process.env.API_KEY
-const express = require('express');
-const app = express();
-function sendTeamsNotification(subject,body){
-  try{
-      // Replace <webhook-url> with the actual webhook URL provided by Teams
-    const webhookUrl = 'https://myfedex.webhook.office.com/webhookb2/9955e2de-3d21-437c-99c7-0b15657457c1@b945c813-dce6-41f8-8457-5a12c2fe15bf/IncomingWebhook/ad3b52249e764827952bd8281193b6ce/50fe584c-09e5-4b10-b92b-ff8d56c63bcd';
+const express = require('express')
+const app = express()
+
+/**
+ * SEND TEAMS NOTIFICATION
+ * Sets the message template and sends the teams notification of a confirmed pickup.
+ *
+ * @param {any} subject
+ * @param {any} body
+ * @returns {any}
+ */
+function sendTeamsNotification (subject, body) {
+  try {
+    // Replace <webhook-url> with the actual webhook URL provided by Teams
+    const webhookUrl =
+      'https://myfedex.webhook.office.com/webhookb2/9955e2de-3d21-437c-99c7-0b15657457c1@b945c813-dce6-41f8-8457-5a12c2fe15bf/IncomingWebhook/ad3b52249e764827952bd8281193b6ce/50fe584c-09e5-4b10-b92b-ff8d56c63bcd'
     // Define the message payload
     const payload = {
-      "@type": "MessageCard",
-      "@context": "http://schema.org/extensions",
-      "themeColor": "0072C6",
-      "title": subject,
-      "text":body,
-      "summary": body,
-      "sections": [
+      '@type': 'MessageCard',
+      '@context': 'http://schema.org/extensions',
+      themeColor: '0072C6',
+      title: subject,
+      text: body,
+      summary: body,
+      sections: [
         {
-          "activityTitle": subject,
-          "activitySubtitle": "Sent by DCC - FedEx",
-          "activityImage": "https://www.fedex.com/content/dam/fedex-com/logos/logo.png"
+          activityTitle: subject,
+          activitySubtitle: 'Sent by DCC - FedEx',
+          activityImage:
+            'https://www.fedex.com/content/dam/fedex-com/logos/logo.png'
         }
       ]
-    };
+    }
     // Send a POST request to the webhook URL with the message payload
-    axios.post(webhookUrl, payload)
+    axios
+      .post(webhookUrl, payload)
       .then(response => {
-        console.log('Message sent:', response.data);
+        console.log('Message sent:', response.data)
       })
       .catch(error => {
-        console.error('Error sending message:', error);
-      });
-  }catch(ex){
-    console.error("something went wrong when sending teams notifications..."+ex.message);
+        console.error('Error sending message:', error)
+      })
+  } catch (ex) {
+    console.error(
+      'something went wrong when sending teams notifications...' + ex.message
+    )
   }
 }
-function sendNotificationEmail(subject,body){
+
+/**
+ * SEND NOTIFICATION EMAIL
+ * Sends an email notification of a confirmed pickup.
+ *
+ * @param {string} subject
+ * @param {string} body
+ */
+function sendNotificationEmail (subject, body) {
   try {
-  const client = Sib.ApiClient.instance
-  const apiKey = client.authentications['api-key']
-  apiKey.apiKey = process.env.API_KEY
-  const tranEmailApi = new Sib.TransactionalEmailsApi()
-  const sender = {
+    const client = Sib.ApiClient.instance
+    const apiKey = client.authentications['api-key']
+    apiKey.apiKey = process.env.API_KEY
+    const tranEmailApi = new Sib.TransactionalEmailsApi()
+    const sender = {
       email: 'makoken.parkmobile@gmail.com',
-      name: 'FedEx Virtual Assistant',
-  }
-  const receivers = [
+      name: 'FedEx Virtual Assistant'
+    }
+    const receivers = [
       {
-          email: 'acwillemse@gmail.com',
-      },
-  ]
-  tranEmailApi
+        email: 'acwillemse@gmail.com'
+      }
+    ]
+    tranEmailApi
       .sendTransacEmail({
-          sender,
-          to: receivers,
-          subject: subject,
-          textContent: body
+        sender,
+        to: receivers,
+        subject: subject,
+        textContent: body
       })
       .then(console.log)
       .catch(console.log)
   } catch (error) {
-    console.error("something went wrong when sending email notifications..."+ex.message);
+    console.error(
+      'something went wrong when sending email notifications...' + ex.message
+    )
   }
-}
-/**
- * GENERATE SHIPMENT NUMBER
- */
-function generateShipmentNumber() {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let shipmentNumber = '';
-  for (let i = 0; i < 16; i++) {
-    shipmentNumber += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return shipmentNumber;
 }
 
+/**
+ * GENERATE SHIPMENT NUMBER
+ * Generates a random shipment number to emulate a unique id for a confirmed pickup.
+ */
+function generateShipmentNumber () {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  let shipmentNumber = ''
+  for (let i = 0; i < 16; i++) {
+    shipmentNumber += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    )
+  }
+  return shipmentNumber
+}
 
 /**
  * GENERATE PICKUP DATE
+ * Generates a simulated date for the confirmed pickup.
  */
-function generatePickupDate() {
-  const currentDate = new Date();
-  const pickupDate = new Date(currentDate.getTime() + Math.floor(Math.random() * 3 + 1) * 24 * 60 * 60 * 1000);
-  return pickupDate.toISOString().slice(0, 10);
+function generatePickupDate () {
+  const currentDate = new Date()
+  const pickupDate = new Date(
+    currentDate.getTime() +
+      Math.floor(Math.random() * 3 + 1) * 24 * 60 * 60 * 1000
+  )
+  return pickupDate.toISOString().slice(0, 10)
 }
-
-
-/**
- * SEND EMAIL
- */
-function sendEmail(subject, body) {
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'va.nuance.email.sender@gmail.com',
-      pass: process.env.email_password
-    }
-  });
-
-  var mailOptions = {
-    from: 'va.nuance.email.sender@gmail.com',
-    to: 'bfb0a43e.myfedex.onmicrosoft.com@amer.teams.ms',
-    subject: subject,
-    text: body
-  };
-  console.log("sending email via gmail user email is va.nuance.email.sender@gmail.com and password is "+process.env.email_password);
-  console.log("email dump"+JSON.stringify(mailOptions));
-  // transporter.sendMail(mailOptions, function (error, info) {
-  //   if (error) {
-  //     console.log(error);
-  //   } else {
-  //     console.log('Email sent: ' + info.response);
-  //   }
-  // });
-}
-
-
-/**
- * HELLO WORLD
- */
-app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
 
 
 /**
  * LOGIN
+ * Mock account API returning account data based on input.
  */
 app.post('/login', (req, res) => {
-  const { fdx_login } = req.body;
+  const { fdx_login } = req.body
   let result = {
     isLoggedIn: false,
-    returnCode: 0,
-  };
+    returnCode: 0
+  }
 
+  // Dummy account for Jane Doe
   if (fdx_login && fdx_login.startsWith('ssodrt-88')) {
-    result.isLoggedIn = true;
+    result.isLoggedIn = true
     result.userDetails = {
       email: 'jane.doe@mail.com',
       firstName: 'Jane',
       lastName: 'Doe',
       phoneNumber: '+31687654321'
-    };
+    }
+  // Dummy account for Fred Smith
   } else if (fdx_login && fdx_login.startsWith('ssodrt-1337')) {
-    result.isLoggedIn = true;
+    result.isLoggedIn = true
     result.userDetails = {
       email: 'fred.smith@fedex.com',
       firstName: 'Fred',
       lastName: 'Smith',
       phoneNumber: '+1123456789'
-    };
+    }
+  // Catch other set 'cookies' with a dummy account (John Doe)
   } else if (fdx_login && fdx_login.startsWith('ssodrt-')) {
-    result.isLoggedIn = true;
+    result.isLoggedIn = true
     result.userDetails = {
       email: 'john.doe@mail.com',
       firstName: 'John',
       lastName: 'Doe',
       phoneNumber: '+31612345678'
-    };
+    }
   }
 
-  res.setHeader('Content-Type', 'application/json');
-  res.json(result);
-});
-
-
-/**
- * PICKUP (GET)
- */
-app.get('/pickup', (req, res) => {
-  let cookies = req.headers.cookie ? req.headers.cookie.substring(0, 50) : "No cookies present";
-  let result = {
-    accountType: "individual", // options: ["individual", "business"]
-    isLoggedIn: true, // boolean
-    returnCode: 0,
-    shipmentPostalCode: "1000AA", // options: Dutch zip codes (regex: ^\d{4}[A-Za-z]{2}$ )
-    shipmentType: "international", // eligible for pickup: ["domestic", "return", "international"]. uneligible: ["freight", "dangerous"]
-    cookies: cookies // cookies object or "No cookies present"
-  };
-  res.setHeader('Content-Type', 'application/json');
-  res.json(result);
-});
-
+  res.setHeader('Content-Type', 'application/json')
+  res.json(result)
+})
 
 /**
  * PICKUP (POST)
+ * Based on specific input this returns mock results.
  *
  * new property in result object called actionRecommendation with default value "VA"
  * new property in result object called isLoggedIn with default value false
@@ -199,8 +176,13 @@ app.get('/pickup', (req, res) => {
  * if fdx_login contains a value and starts with "ssodrt-" then set result.isLoggedIn to true and introduce a new complex object type under result object called userDetails. Set result.userDetails.firstName to "John" and result.userDetails.lastName to "Doe".
  */
 app.post('/pickup', (req, res) => {
-  // console.log("req.body is::: "+req.body);
-  const { trackingNumber, shipmentAddressFrom, shipmentAddressTo, shipmentWeight, fdx_login } = req.body;
+  const {
+    trackingNumber,
+    shipmentAddressFrom,
+    shipmentAddressTo,
+    shipmentWeight,
+    fdx_login
+  } = req.body
   let result = {
     accountType: 'individual',
     actionRecommendation: 'VA',
@@ -209,69 +191,48 @@ app.post('/pickup', (req, res) => {
     shipmentAddressFrom: shipmentAddressFrom ? shipmentAddressFrom : '',
     shipmentAmount: 0, // amount of packages
     shipmentPostalCode: '1000AA',
-    shipmentType: 'international',
-  };
+    shipmentType: 'international'
+  }
 
   if (trackingNumber) {
     if (trackingNumber.startsWith('100')) {
-      result.shipmentAddressFrom = 'Amsterdam';
-      result.shipmentAmount = 3;
+      result.shipmentAddressFrom = 'Amsterdam'
+      result.shipmentAmount = 3
       result.shipmentInstructions = 'Use video doorbell on the left' // not functional yet in Mix
-      result.shipmentPostalCode = '1051GM';
-      result.shipmentType = 'domestic';
-
+      result.shipmentPostalCode = '1051GM'
+      result.shipmentType = 'domestic'
     } else if (trackingNumber.startsWith('200')) {
-      result.accountType = 'business';
-      result.shipmentAmount = 2;
-      result.shipmentPostalCode = '2132LS';
-      result.shipmentType = 'international';
-
+      result.accountType = 'business'
+      result.shipmentAmount = 2
+      result.shipmentPostalCode = '2132LS'
+      result.shipmentType = 'international'
     } else if (trackingNumber.startsWith('900')) {
-      result.actionRecommendation = 'HumanOperator';
-      result.shipmentAmount = 1;
-      result.shipmentPostalCode = '8888XX';
-      result.shipmentType = 'dangerous';
+      result.actionRecommendation = 'HumanOperator'
+      result.shipmentAmount = 1
+      result.shipmentPostalCode = '8888XX'
+      result.shipmentType = 'dangerous'
     }
   }
 
-  /*
-  if (shipmentAddressTo) {
-    result.shipmentAddressTo_verify = shipmentAddressTo + "--received";
-  }else{
-    result.shipmentAddressTo_verify = "no-to-address--received";
-  }
-
-  if (shipmentWeight) {
-    result.weight_verify = shipmentWeight+ "--received. request body everything-->>>"+JSON.stringify(req.body);
-  }else{
-    result.weight_verify = "no-to-weight_verify--received....request body everything-->>>"+JSON.stringify(req.body);
-  }
-
-  if (shipmentAddressFrom) {
-    result.from_address_verify = shipmentAddressFrom + "--received";
-    console.log("sending email... from address is set. "+ shipmentAddressFrom);
-
-    //sendEmail("Nuance Mix - Schedule Pickup", "response data is "+JSON.stringify(result));
-  } else {
-    result.from_address_verify = "no-from-address--received";
-  }*/
-
-  res.setHeader('Content-Type', 'application/json');
-  res.json(result);
-});
-
+  res.setHeader('Content-Type', 'application/json')
+  res.json(result)
+})
 
 /**
  * CONFIRM PICKUP
  * Pickup confirmation, processes the pickup data into a sentence.
  */
 app.post('/confirmpickup', (req, res) => {
-  const { trackingNumber, shipmentAddressFrom, shipmentAddressTo, shipmentAmount } = req.body;
-  let pickupDate = generatePickupDate();
-  let shipmentNumber= generateShipmentNumber();
-  const confirmationMessage = `🚚 Great news! Your shipment (${trackingNumber}), consisting of ${shipmentAmount} packages is scheduled for pickup on ${pickupDate} in ${shipmentAddressFrom} and will be sent to ${shipmentAddressTo}. Your pickup reference is ${shipmentNumber}. Please check your email inbox to confirm or change pickup details.`;
-  const confirmationEmail =
-    `<h1>🚚 Thanks for scheduling a pickup with us.</h1>
+  const {
+    trackingNumber,
+    shipmentAddressFrom,
+    shipmentAddressTo,
+    shipmentAmount
+  } = req.body
+  let pickupDate = generatePickupDate()
+  let shipmentNumber = generateShipmentNumber()
+  const confirmationMessage = `🚚 Great news! Your shipment (${trackingNumber}), consisting of ${shipmentAmount} packages is scheduled for pickup on ${pickupDate} in ${shipmentAddressFrom} and will be sent to ${shipmentAddressTo}. Your pickup reference is ${shipmentNumber}. Please check your email inbox to confirm or change pickup details.`
+  const confirmationEmail = `<h1>🚚 Thanks for scheduling a pickup with us.</h1>
     <p>ℹ️ <i> We will inform you of an estimated pickup time window 1 day beforehand</i></p>
     <p>
       <strong>Pick-up reference number</strong> ${shipmentNumber}<br/>
@@ -284,7 +245,7 @@ app.post('/confirmpickup', (req, res) => {
     <p>
       <a href="https://fedex.com/en-gb/customer-support.html"><strong>Click here</strong> to confirm your pickup.</a>
     </p>`
-    const result = {
+  const result = {
     actionRecommendation: 'VA',
     trackingNr: trackingNumber,
     addressFrom: shipmentAddressFrom,
@@ -293,19 +254,17 @@ app.post('/confirmpickup', (req, res) => {
     confirmationMessage: confirmationMessage,
     pickupDate: pickupDate,
     returnCode: 0,
-    shipmentNumber: shipmentNumber,
-  };
+    shipmentNumber: shipmentNumber
+  }
 
-  const notificationSubject =  `Pickup Confirmation for tracking number ${trackingNumber}`;
+  const notificationSubject = `Pickup Confirmation for tracking number ${trackingNumber}`
   sendNotificationEmail(notificationSubject, confirmationEmail)
-  sendTeamsNotification(notificationSubject,confirmationMessage)
+  sendTeamsNotification(notificationSubject, confirmationMessage)
 
-  res.json(result);
-});
+  res.json(result)
+})
 
-
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
-});
+  console.log(`Server listening on port ${PORT}...`)
+})
